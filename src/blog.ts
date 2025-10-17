cd3189c88eccdb9e5883b57856e1d4e0ecdfc80d
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2025-10-16 21:52:38 -0700

    feat: intial files and Blog setup

---
+++ src/blog.ts
@@ -0,0 +1,31 @@
+type Blog = {
+    title: string,
+    date: string
+    description: string
+    image: string
+    imageAlt: string
+    slug: string
+}
+
+
+const Blogs: Blog[] = [
+    {
+        title: "Blog 1",
+        date: "10-15-2025",
+        description: "This is the description of blog 1",
+        image: "./",
+        imageAlt: "Blog 1",
+        slug: "blog-1"
+    },
+    {
+        title: "Blog 2",
+        date: "10-16-2025",
+        description: "This is the description of blog 2",
+        image: "./",
+        imageAlt: "Blog 2",
+        slug: "blog-2"
+    }
+]
+
+
+
