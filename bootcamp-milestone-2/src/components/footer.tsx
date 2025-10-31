2ebcf26897e4ed0a2b9b66d66d1cc4d6821b75cd
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2025-10-31 01:07:51 -0700

    feat: footer component

---
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -0,0 +1,8 @@
+import style from "./footer.module.css";
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+            © 2023 Aryan's Website | All Rights Reserved
+        </footer>
+    );
+}
