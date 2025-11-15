bf640c22b7732b4b31def12ff2e90c90c39ab4f2
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2025-11-15 01:48:44 -0800

    feat: use mongo entries for blog preview page

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,22 +1,22 @@
 import style from "./blogPreview.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import type { Blog } from "@/app/blogData";
+import Blog from "@/database/blogSchema";
 
 export default function BlogPreview(props: Blog) {
     return (
         <div>
             <h1>{props.title}</h1>
-            <p>{props.date}</p>
+            <p>{props.date.toLocaleDateString("en-US")}</p>
             <p>{props.description}</p>
             <Image
                 src={props.image}
-                alt={props.imageAlt}
+                alt={props.image_alt}
                 height="200"
                 width="300"
             />
 
-            <Link href={`/blogs/${props.slug}.html`}>LEARN MORE</Link>
+            <p><Link href={`/blogs/${props.slug}`}>LEARN MORE</Link></p>
         </div>
     );
 }
